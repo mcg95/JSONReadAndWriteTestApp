4d60bc5ab007ddcfa909d9792223d6ca05a72e1b
Mewan Chathuranga <mewan@hauz.com.my>
2019-10-07 01:03:34 +0800

    Completed UI Creation

---
+++ JSONReadAndWriteTestApp/CustomViews/CustomCells/ContactDetailCell.swift
@@ -0,0 +1,20 @@
+//
+//  ContactDetailCell.swift
+//  JSONReadAndWriteTestApp
+//
+//  Created by HAUZ MacBook Pro 3 on 07/10/2019.
+//
+
+import UIKit
+
+class ContactDetailCell: UITableViewCell{
+    
+    @IBOutlet weak var contactDetailLabel: UILabel!
+    @IBOutlet weak var contactDetailTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+}
