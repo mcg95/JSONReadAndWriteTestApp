c6e2c606bd67744c196cdfd357e2807bd54c08c3
Mewan Chathuranga <mewan@hauz.com.my>
2019-10-07 00:22:14 +0800

    Initial Commit

---
+++ JSONReadAndWriteTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONReadAndWriteTestApp
+//
+//  Created by HAUZ MacBook Pro 3 on 07/10/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
